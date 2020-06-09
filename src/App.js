4eed9ddd7a245fd38cdf30597abab0f430102c81
Frederick-88 <frederickfd88@gmail.com>
2020-06-09 11:29:44 +0700

    update weatherpedia

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [dataWeather, setDataWeather] = useState("");
+  useEffect(() => {
+    axios
+      .get(
+        "http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02"
+      )
+      .then((response) => {
+        console.log(response);
+        setDataWeather(response.data);
+      });
+  });
+  console.log(dataWeather);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div>
+      <div class="jumbotron">
+        <h1 class="display-4">Hello, world!</h1>
+        <p class="lead">
+          This is a simple hero unit, a simple jumbotron-style component for
+          calling extra attention to featured content or information.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <hr class="my-4" />
+        <div></div>
+        <p>
+          It uses utility classes for typography and spacing to space content
+          out within the larger container.
+        </p>
+        <a class="btn btn-primary btn-lg" href="#" role="button">
+          Learn more
         </a>
-      </header>
+      </div>
+      <p>{`data${dataWeather}`}</p>
     </div>
   );
 }
